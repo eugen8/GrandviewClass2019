b87dbe358b916e24f4aeaeaa6484c36d74bdb87f
Eugen Burianov <netboymd@yahoo.com>
2019-09-20 05:40:44 -0500

    updates for lesson 7

--- Lesson7/index.php
+++ Lesson7/index.php
@@ -10,7 +10,36 @@
 <h1>Lesson7</h1>
 <?php
 include_once("util.inc.php");
+
+include("util.inc.php");
+include("util.inc.php");
+include("util.inc.php");
+
+$name = "";
+$nameExists = true;
+
+$m = "";
+
+
+if($_SERVER['REQUEST_METHOD']=='POST'){
+    $name = $_POST["name"];
+    if( empty(  trim($name)  ) ){
+        $nameExists = false;
+    }
+}
+
 ?>
+<br/>
+Form
+
+
+
+<form action="index.php" method="POST">
+    <p><input name="name" type="text" placeholder="first name" style="background-color:<?php  echo $nameExists?"white":"red";  ?>" value="<?php echo $name ?>">   </p>
+    <p><input name="last_name" type="text" placeholder="last name"></p>
+    <p><Button type="submit">Submit</Button></p>
+    
+</form>
 
 
 
